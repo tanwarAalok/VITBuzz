09d437df8c10d3f9a1b01c88246700ba5efcf9e4
Aalok Tanwar <aaloktanwar88@gmail.com>
2023-02-01 00:36:57 +0530

    navbar and prof page done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import styles from '@/styles/Navbar.module.css'
+import Link from 'next/link'
+
+const Navbar = () => {
+  return (
+    <div className={styles.navbarComponent}>
+      <div className={styles.logoBox}>
+        <Link href="placements">Placements</Link>
+        <Link href="faculty">Faculty</Link>
+      </div>
+      <div className={styles.logo}>
+        VIT<span>Buzz</span>
+      </div>
+      <div className={styles.logoBox}>
+        <Link href="clubs">Clubs</Link>
+        <Link href="papers">Papers</Link>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar
