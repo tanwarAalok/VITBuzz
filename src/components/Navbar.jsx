4924c69e1a064c425732ee42698173638552516d
Aalok Tanwar <aaloktanwar88@gmail.com>
2023-06-20 03:44:23 +0530

    bug fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ import {LogInIcon, LogOutIcon} from "@/utils/Icons";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-
+import Hamberger from "../assets/hamberger.png";
 
 const NavbarComponent = () => {
   const { data: session } = useSession();
