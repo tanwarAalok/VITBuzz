45fef6e67a97da580ef2636c6dfc2beb96988341
Aalok Tanwar <aaloktanwar88@gmail.com>
2023-06-20 03:21:25 +0530

    first page made responsive

---
+++ src/components/ClubActivityCarousel.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Carousel } from "react-bootstrap";
+import { useState } from "react";
+import RightArrow from "../assets/rightArrow.png";
+import LeftArrow from "../assets/left-arrow.png"
+
+const ClubActivityCarousel = ({styles}) => {
+    const [index, setIndex] = useState(0);
+    const handleSelect = (selectedIndex, e) => {
+        setIndex(selectedIndex);
+    };
+
+    return (
+        <Carousel
+            autoPlay={false}
+            variant="dark"
+            activeIndex={index}
+            onSelect={handleSelect}
+            className={styles.carouselParent}
+        >
+            {/*{apiData?.map((video) => (*/}
+                <Carousel.Item className={styles.carouselItem}>
+                    <iframe
+                        src="https://www.youtube.com/embed/ZzE2ejjyEKM"
+                        title="YouTube video player"
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        allowFullScreen
+                    ></iframe>
+                </Carousel.Item>
+            <Carousel.Item className={styles.carouselItem}>
+                    <iframe
+                        src="https://www.youtube.com/embed/ZzE2ejjyEKM"
+                        title="YouTube video player"
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        allowFullScreen
+                    ></iframe>
+                </Carousel.Item>
+            {/*))}*/}
+        </Carousel>
+    );
+};
+
+export default ClubActivityCarousel;
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -5,13 +5,14 @@ import vitImg from "../assets/vit1.png";
 import leftArrow from "../assets/leftArrow.png";
 import rightArrow from "../assets/rightArrow.png";
 import { Spinner } from 'react-bootstrap';
+import ClubActivityCarousel from "@/components/ClubActivityCarousel";
 
 const LandingPage = ({styles, topFaculty, isLoading}) => {
   return (
     <div className={styles.mainPage}>
       <div className={styles.vitImage}>
         <Image src={vitImg} alt="vitImage" />
-        <div className={styles.vitb}>
+        <div className={styles.vit_slogan}>
           <div>VITB</div>
           <div>
             the best place <br /> to be...
@@ -39,19 +40,7 @@ const LandingPage = ({styles, topFaculty, isLoading}) => {
       <div className={styles.clubSection}>
         <h2>Recent Club Activities</h2>
         <div className={styles.cscp}>
-          <Image src={leftArrow} alt="left Arrow" width="50" height="50" />
-          <div className={styles.cvc}>
-            <iframe
-              width="560"
-              height="315"
-              src="https://www.youtube.com/embed/ZzE2ejjyEKM"
-              title="YouTube video player"
-              frameborder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              allowfullscreen
-            ></iframe>
-          </div>
-          <Image src={rightArrow} alt="right Arrow" width="50" height="50" />
+            <ClubActivityCarousel styles={styles}/>
         </div>
       </div>
     </div>
